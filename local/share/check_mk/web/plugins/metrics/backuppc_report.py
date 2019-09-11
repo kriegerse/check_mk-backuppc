ec12d4a5be3b4038099b5ec74ec542a1bade111c
kriegerse <s_krieger@web.de>
2019-09-11 20:14:17 +0000

    started check logic for summary check

--- local/share/check_mk/web/plugins/metrics/backuppc_report.py
+++ local/share/check_mk/web/plugins/metrics/backuppc_report.py
@@ -1,28 +1,28 @@
 ## BackupPC Host Summary Statistic 
 # define metrics colot/unit
-metric_info['total_host_count'] = {
-    'title': _('Total hosts'),
+metric_info['hosts'] = {
+    'title': _('Hosts'),
     'unit': 'count',
     'help': _('Total number of hosts with backup'),
     'color': indexed_color(4,4),
 }
 
-metric_info['total_disabled_count'] = {
-    'title': _('Disabled hosts'),
+metric_info['hosts_disabled'] = {
+    'title': _('Hosts disabled'),
     'unit': 'count',
     'help': _('Number of hosts with disabled backup'),
     'color': indexed_color(1,4),
 }
 
-metric_info['total_tooold_count'] = {
-    'title': _('Hosts with too old Backup'),
+metric_info['hosts_overage'] = {
+    'title': _('Hosts overaged'),
     'unit': 'count',
     'help': _('Number of hosts with too old backups'),
     'color': indexed_color(2,4),
 }
 
-metric_info['total_progress_count'] = {
-    'title': _('Host in progress'),
+metric_info['hosts_progress'] = {
+    'title': _('Hosts in progress'),
     'unit': 'count',
     'help': _('Number of hosts with backup in progress'),
     'color': indexed_color(3,4),
@@ -31,34 +31,34 @@ metric_info['total_progress_count'] = {
 
 # define graph
 graph_info.append({
-   'title': _('BackupPC Hosts Summary'),
+   'title': _('BackupPC Host Summary'),
    'metrics': [
-       ( 'total_tooold_count',      'stack'),
-       ( 'total_disabled_count',    'stack'),
-       ( 'total_progress_count',    'stack'),
-       ( 'total_host_count',        'line'),
+       ( 'hosts_overage',      'stack'),
+       ( 'hosts_disabled',    'stack'),
+       ( 'hosts_progress',    'stack'),
+       ( 'hosts',        'line'),
    ],
 })
 
 
 ## BackupPC Backup Summary
 # define metrics colot/unit
-metric_info['total_backup_count'] = {
+metric_info['bkp_count'] = {
     'title': _('Total backups'),
     'unit': 'count',
     'help': _('Total number of backups'),
     'color': indexed_color(1,3),
 }
 
-metric_info['total_error_count'] = {
+metric_info['bkp_errors'] = {
     'title': _('Total errors'),
     'unit': 'count',
     'help': _('Total number of erroneous backups'),
     'color': indexed_color(2,3),
 }
 
-metric_info['total_partial_count'] = {
-    'title': _('Total partial backups'),
+metric_info['bkp_partial'] = {
+    'title': _('Total partial'),
     'unit': 'count',
     'help': _('Total number of partial backups'),
     'color': indexed_color(3,3),
@@ -69,8 +69,8 @@ metric_info['total_partial_count'] = {
 graph_info.append({
     'title': _('BackupPC Backup Summary'),
     'metrics': [
-        ( 'total_backup_count',     'area'),
-        ( 'total_error_count' ,     'area'),
-        ( 'total_partial_count',    'area'),
+        ( 'bkp_count',     'area'),
+        ( 'bkp_errors' ,     'area'),
+        ( 'bkp_partial',    'area'),
     ],
 })
